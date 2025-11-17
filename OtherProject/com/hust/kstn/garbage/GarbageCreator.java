ec1c74b4412610ce956de4462f584b8021b3f75b
Tam Ta <tamta@Tams-MacBook.local>
2025-11-17 14:30:47 +0700

    section 3.2

---
+++ OtherProject/com/hust/kstn/garbage/GarbageCreator.java
@@ -0,0 +1,26 @@
+package com.hust.kstn.garbage;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class GarbageCreator {
+    public static void main(String[] args) {
+        try {
+            String filename = "test.exe";   
+            byte[] inputBytes = Files.readAllBytes(Paths.get(filename));
+
+            long startTime = System.currentTimeMillis();
+
+            String outputString = "";
+            for (byte b : inputBytes) {
+                outputString += (char) b;   
+            }
+
+            long endTime = System.currentTimeMillis();
+            System.out.println("GarbageCreator time: " + (endTime - startTime) + " ms");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
